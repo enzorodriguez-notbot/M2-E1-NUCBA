5158f7da7bea43e8c0b61c83b09284940f7a26ea
enzorodriguez-notbot <141461776+enzorodriguez-notbot@users.noreply.github.com>
2023-11-15 12:15:00 -0300

    1

--- assets/main.js
+++ assets/main.js
@@ -23,3 +23,15 @@ if (parte2n1 >= parte2n2) {
 //espacio
 //espacio
 //parte3
+let parte3 = prompt("Introduce un numero y te dire si es multiplo de 5");
+if (parte3 % 5) {
+  console.log("El numero " + parte3 + " no es multiplo de 5");
+} else {
+  console.log("El numero " + parte3 + " es multiplo de 5");
+}
+//espacio
+//espacio
+//parte4
+let parte4 = prompt("Introduce un numero");
+for (let i = 0; (i = parte4); i++) if ((i = parte4)) break;
+print(i);
