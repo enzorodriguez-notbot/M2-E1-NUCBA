e5bbc6c59fdbb671fd366ae54e9871d97be60690
enzorodriguez-notbot <141461776+enzorodriguez-notbot@users.noreply.github.com>
2023-11-15 11:38:12 -0300

    a

---
+++ assets/main.js
@@ -0,0 +1,25 @@
+//parte1
+let parte1;
+parte1 = prompt("introduce un numero y te dire si es par o impar");
+if (parte1 % 2 == 0) {
+  console.log("El numero " + parte1 + " es par");
+} else {
+  console.log("El numero " + parte1 + " es impar");
+}
+//espacio
+//espacio
+//parte2
+let parte2n1;
+let parte2n2;
+parte2n1 = prompt(
+  "introduce un primer numero y te dire si es mayor igual o menor que el segundo"
+);
+parte2n2 = prompt("introduce un segundo numero para la comparacion");
+if (parte2n1 >= parte2n2) {
+  console.log("El numero " + parte2n1 + " es mayor o igual que " + parte2n2);
+} else {
+  console.log("El numero " + parte2n1 + " es menor que " + parte2n2);
+}
+//espacio
+//espacio
+//parte3
